f5d5bca0446d6dc7c027f1d9b71ca1159cd7512f
Anish <a4anishm@gmail.com>
2011-08-30 06:51:59 +0000

    Created empty rails project

---
+++ meethodu/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meethodu::Application.config.secret_token = 'a45df52c69450070d74d0f91a97b3721ca5cf784ad7e5c613365422368fe20de94bc7fa25715f6a9a3b7fd123ec3d1ab3ab4b08e56f609138141f5cdd03d18b5'
